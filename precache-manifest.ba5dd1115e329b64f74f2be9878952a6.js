e7ffc6d462cb706b2ed70d9b483f88f9a88cda22
hocineimail <hocineismail75@gmail.com>
2019-03-17 17:21:31 +0100

    Updates

---
+++ precache-manifest.ba5dd1115e329b64f74f2be9878952a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38bdb6096543dfc8f937",
+    "url": "/Calculator-app-React.js/static/js/runtime~main.c54015e7.js"
+  },
+  {
+    "revision": "6d60efd17fa149630193",
+    "url": "/Calculator-app-React.js/static/js/main.b6ff63c7.chunk.js"
+  },
+  {
+    "revision": "219d8bcf671cfe9a440c",
+    "url": "/Calculator-app-React.js/static/js/2.9d208366.chunk.js"
+  },
+  {
+    "revision": "6d60efd17fa149630193",
+    "url": "/Calculator-app-React.js/static/css/main.8d036f49.chunk.css"
+  },
+  {
+    "revision": "219d8bcf671cfe9a440c",
+    "url": "/Calculator-app-React.js/static/css/2.5db7f7e8.chunk.css"
+  },
+  {
+    "revision": "81acdc7b9452036aa1cecb17c9666bc5",
+    "url": "/Calculator-app-React.js/index.html"
+  }
+];
