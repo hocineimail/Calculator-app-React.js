3003f7398a4e9cf67df05eaf6003752780293680
hocineimail <hocineismail75@gmail.com>
2019-03-17 17:13:03 +0100

    Add state && function for Calculator

--- src/App.js
+++ src/App.js
@@ -3,10 +3,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Buttons from './components/Buttons'
-
+import Result from './components/Result'
 import 'bootstrap/dist/css/bootstrap.min.css'
 class App extends Component {
 
+
+    state = {
+        result: ""
+    }
+
+
+onClick = button => {
+console.log(button)
+    if(button === "="){
+        this.calculate()
+    }
+
+    else if(button === "C"){
+        this.reset()
+    }
+    else if(button === "CE"){
+        this.backspace()
+    }
+    
+    else {
+     
+        this.setState({
+            result: this.state.result + button
+        }) }
+    
+};
+
+
+calculate = () => {
+    try {
+        this.setState({
+            // eslint-disable-next-line
+            result: (eval(this.state.result) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+
+    }
+};
+
+reset = () => {
+    this.setState({
+        result: ""
+    })
+};
+
+backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+};
 
 render() {
     return (
@@ -14,7 +67,8 @@ render() {
            <header className="App-header">
             <div className="calculator-body">
                 <h2> Calculator</h2>
-                <Buttons />
+                <Result result={this.state.result}/>
+                <Buttons onClick={this.onClick}/>
             </div>
             </header>
         </div>
